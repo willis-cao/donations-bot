fed3046c5e10aa4aa9584b2fc5f8b266b91ea7a1
Willis Cao <wcao2012@gmail.com>
2023-09-22 18:10:14 -0700

    Updated with more responses to "league?"

--- src/index.js
+++ src/index.js
@@ -12,10 +12,22 @@ const client = new Client({
 });
 
 client.on('messageCreate', async (message) => {
+    if(message.author.bot) return;
+    
+    if (message.content.includes("i-")) {
+        message.channel.send("i-");
+    }
+
+    // =======================
+    // @ mention replies below
+    // =======================
+
     if (!message.mentions.has(client.user.id)) return;
     
-    if (message.content.includes("league?")) {
-        if (message.author.id == process.env.SILENTCTRL_ID) {
+    if (message.content.includes("league") || message.content.includes("leg")) {
+        if (message.author.id == process.env.SILENTCTRL_ID ||
+            message.author.id == process.env.WULONG_ID ||
+            message.author.id == process.env.EXPLO_ID) {
             await new Promise(r => setTimeout(r, 5000));
             message.channel.sendTyping();
             await new Promise(r => setTimeout(r, 2500));
@@ -27,6 +39,18 @@ client.on('messageCreate', async (message) => {
             message.channel.sendTyping();
             await new Promise(r => setTimeout(r, 2500));
             message.reply("https://gcdnb.pbrd.co/images/DFgXpJuM7xFd.webp?o=1");
+        }
+        if (message.author.id == process.env.KJKW_ID) {
+            await new Promise(r => setTimeout(r, 2000));
+            message.channel.sendTyping();
+            await new Promise(r => setTimeout(r, 200));
+            message.reply("sure let's solo queue");
+        }
+        if (message.author.id == process.env.LEONA_ID) {
+            await new Promise(r => setTimeout(r, 5000));
+            message.channel.sendTyping();
+            await new Promise(r => setTimeout(r, 2500));
+            message.reply("aren't you playing civ tonight");
         }
     }
         
