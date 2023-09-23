5906a7ca7b59ca8028ac2af24b42f9b634afff62
Willis Cao <wcao2012@gmail.com>
2023-09-22 17:51:06 -0700

    Added "league?" reply feature.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,29 @@ const client = new Client({
     ],
 });
 
+client.on('messageCreate', async (message) => {
+    if (!message.mentions.has(client.user.id)) return;
+    
+    if (message.content.includes("league?")) {
+        if (message.author.id == process.env.SILENTCTRL_ID) {
+            await new Promise(r => setTimeout(r, 5000));
+            message.channel.sendTyping();
+            await new Promise(r => setTimeout(r, 2500));
+            message.reply("gimme 2 min");
+        }
+        if (message.author.id == process.env.DELISI_ID ||
+            message.author.id == process.env.HARUMIN_ID) {
+            await new Promise(r => setTimeout(r, 5000));
+            message.channel.sendTyping();
+            await new Promise(r => setTimeout(r, 2500));
+            message.reply("https://gcdnb.pbrd.co/images/DFgXpJuM7xFd.webp?o=1");
+        }
+    }
+        
+
+    
+});
+
 eventHandler(client);
 
 client.login(process.env.TOKEN);
